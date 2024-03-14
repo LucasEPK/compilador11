bdcdeeb4c1695db74838ede60fe73d17728e887e
Lucas Moyano <lucasmoyano11@outlook.it>
2024-03-14 11:53:25 -0300

    empecé a implementar el automata (EOF y NewLine)

--- Compiler/src/main/java/LexicalAnalyzer/LexicalAnalyzer.java
+++ Compiler/src/main/java/LexicalAnalyzer/LexicalAnalyzer.java
@@ -1,4 +1,140 @@
 package LexicalAnalyzer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LexicalAnalyzer {
+    //Acá se definen todos los tokens como constantes para simplificar el cambio de nombres
+    private final String NEW_LINE = "NewLine";
+    private final String EOF = "$EOF$";
+
+    private int currentPos = 0;
+    public LexicalAnalyzer (String file){
+        List<String> tokens = new ArrayList<String>();
+
+        String token = s0(file);
+        while (! token.equals(EOF)){
+            // no se si hay que contemplar si termina sin $EOF$, dudo
+            tokens.add(token);
+
+            token = s0(file);
+        }
+
+        tokens.add(token); // agrega token EOF a la lista de tokens
+    }
+
+    /**
+     * Este es el comienzo del automata
+     * @author Lucas Moyano
+     * @param file es un string que tiene todo el contenido del archivo que se se está leyendo
+     * */
+    private String s0(String file){
+        String token = null;
+        switch (file.charAt(currentPos)) {
+            case '\\':
+                currentPos += 1;
+                token = s1(file);
+                break;
+            case '$':
+                currentPos += 1;
+                token = s50(file);
+                break;
+            default:
+                //TODO: Acá tendria que largar error porque no se encontró un caracter valido
+        }
+        return token;
+    }
+
+    /**
+     * Con este metodo se entra en los nodos del automata que corresponden
+     * a lexemas que empiezan con \
+     * @author Lucas Moyano
+     * */
+    private String s1(String file){
+        String token = null;
+        switch (file.charAt(currentPos)) {
+            case 'n':
+                token = NEW_LINE;
+                currentPos += 1;
+                break;
+            default:
+                // TODO: acá debería arrojar error
+        }
+
+        return token;
+    }
+
+    /**
+     * Con este metodo se entra en los nodos del automata
+     * que van a terminar en $EOF$
+     * @author Lucas Moyano
+     * */
+    private String s50(String file) {
+        String token = null;
+        switch (file.charAt(currentPos)) {
+            case 'E':
+                currentPos += 1;
+                token = s51(file);
+                break;
+            default:
+                // TODO: acá debería arrojar error
+        }
+
+        return token;
+    }
+
+    /**
+     * Camino hacia $EOF$
+     * @author Lucas Moyano
+     * */
+    private String s51(String file) {
+        String token = null;
+        switch (file.charAt(currentPos)) {
+            case 'O':
+                currentPos += 1;
+                token = s52(file);
+                break;
+            default:
+                // TODO: acá debería arrojar error
+        }
+
+        return token;
+    }
+
+    /**
+     * Camino hacia $EOF$
+     * @author Lucas Moyano
+     * */
+    private String s52(String file) {
+        String token = null;
+        switch (file.charAt(currentPos)) {
+            case 'F':
+                currentPos += 1;
+                token = s53(file);
+                break;
+            default:
+                // TODO: acá debería arrojar error
+        }
+
+        return token;
+    }
+
+    /**
+     * Camino hacia $EOF$
+     * @author Lucas Moyano
+     * */
+    private String s53(String file) {
+        String token = null;
+        switch (file.charAt(currentPos)) {
+            case '$':
+                token = EOF;
+                currentPos += 1;
+                break;
+            default:
+                // TODO: acá debería arrojar error
+        }
+
+        return token;
+    }
+
 }
---
+++ Compiler/src/main/java/xd.java
@@ -0,0 +1,7 @@
+import LexicalAnalyzer.LexicalAnalyzer;
+
+public class xd {
+    public static void main(String args[]) {
+        LexicalAnalyzer xd = new LexicalAnalyzer("\\n$EOF$");
+    }
+}
