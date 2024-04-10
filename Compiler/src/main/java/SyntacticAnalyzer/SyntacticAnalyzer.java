43d7caa794f9bf96d0cfe83aa4a3954a1581030a
Yeu07 <yeumensilva9@gmail.com>
2024-04-10 20:39:49 -0300

    Things

---
+++ Compiler/src/main/java/SyntacticAnalyzer/SyntacticAnalyzer.java
@@ -0,0 +1,15 @@
+package SyntacticAnalyzer;
+
+import LexicalAnalyzer.LexicalAnalyzer;
+import LexicalAnalyzer.Token;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class SyntacticAnalyzer {
+
+    private Token actualToken;
+
+
+}
