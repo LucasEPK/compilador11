8822be0f72529d4ae67b788fa7b4c61842d5daa5
Yeu07 <yeumensilva9@gmail.com>
2024-04-11 15:20:13 -0300

    Arreglado el executor del sintáctico

--- Compiler/src/main/java/SyntacticAnalyzer/SyntacticExecutor.java
+++ Compiler/src/main/java/SyntacticAnalyzer/SyntacticExecutor.java
@@ -20,15 +20,24 @@ import java.util.List;
  */
 public class SyntacticExecutor {
 
-    private  final HashMap<String,String> hashMap = new HashMap<>();
+    private final HashMap<String, String> hashMap = new HashMap<>();
+    private final FileManager fileManager;
+    private  final LexicalAnalyzer lexicalAnalyzer;
+
 
     /**
-     * Constructor que inicia nuestra tablas hash de palabras reservadas
+     * Constructor de la clase que inicia el léxico y la tabla
+     * de palabras reservadas
+     * @param inputPath path del archivo de entrada
+     * @param  outputPath path al archivo de salida
      * @author Yeumen Silva
      */
 
-    public SyntacticExecutor(){
+    SyntacticExecutor(String inputPath, String outputPath) {
         startHashTable();
+        this.fileManager = new FileManager(inputPath, outputPath);
+        String file = this.fileManager.getInputFile();
+        this.lexicalAnalyzer = new LexicalAnalyzer(file);
     }
 
     /**
@@ -37,7 +46,7 @@ public class SyntacticExecutor {
      * @author Yeumen Silva
      */
 
-    private void startHashTable(){
+    private void startHashTable() {
 
         this.hashMap.put("start", "start");
         this.hashMap.put("struct", "struct");
@@ -50,7 +59,8 @@ public class SyntacticExecutor {
         this.hashMap.put("ret", "ret");
         this.hashMap.put("if", "if");
         this.hashMap.put("else", "else");
-        this.hashMap.put("while", "while");;
+        this.hashMap.put("while", "while");
+        ;
         this.hashMap.put("true", "true");
         this.hashMap.put("false", "false");
         this.hashMap.put("fn", "fn");
@@ -63,68 +73,41 @@ public class SyntacticExecutor {
 
     }
 
-
     /**
-     * Método que dado un String de entrada crea una instancia de la clase
-     * FileMannager, recibe los tokens del analizador léxico y llama
-     * a función que guarda los resultados en un archivo de salida
-     * o los imprime por consola
-     * @param inputPath String con ruta del archivo de entrada
-     * @param outputPath String con ruta del archivo de salida
+     * Método que devuelve el next token al analizador sintáctico
+     * @author Yeumen Silva
      */
-    public void startExecution(String inputPath, String outputPath){
-        FileManager fileManager = new FileManager(inputPath, outputPath);
 
-        List<Token> tokens = new ArrayList<Token>();
-        String file = fileManager.getInputFile();
-        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(file);
+    public Token getNextToken() {
 
-         /*
-       Este bloque try/catch es el encargado de ejecutarse siempre
-       y cuando no encontremos un error lexico en la ejecución
-        */
+        Token token = this.lexicalAnalyzer.getNextToken();
 
         try {
-            // Crea una lista de tokens utilizando el LexicalAnalyzer
-            Token token = lexicalAnalyzer.getNextToken();
-            while (! token.getToken().equals(lexicalAnalyzer.getEOF())){
 
-                //Verifico si el lexema esta en la tabla Hash
-                if(this.hashMap.containsKey(token.getLexeme())){
+            // Si no es uno de los tokens válidos, sigo buscando uno
 
-                    //Si esta, entonces es una palabra reservada y cambio su token
+            while (token.getToken().equals(this.lexicalAnalyzer.getBLANK_SPACE()) &&
+                    token.getToken().equals(this.lexicalAnalyzer.getTAB()) &&
+                    token.getToken().equals(this.lexicalAnalyzer.getCARRIAGE_RETURN()) &&
+                    token.getToken().equals(this.lexicalAnalyzer.getNEW_LINE()) &&
+                    token.getToken().equals(this.lexicalAnalyzer.getVERTICAL_TAB()) &&
+                    token.getToken().equals(this.lexicalAnalyzer.getSIMPLE_COMMENT())){
 
-                    token.setToken(token.getLexeme());
-                }
+                token = this.lexicalAnalyzer.getNextToken();
 
-                // Verificamos si no es algunos de los tokens skipeables, si no lo es entonces lo agregamos a la lista de tokens
-                if (! token.getToken().equals(lexicalAnalyzer.getBLANK_SPACE()) &&
-                        ! token.getToken().equals(lexicalAnalyzer.getTAB()) &&
-                        ! token.getToken().equals(lexicalAnalyzer.getCARRIAGE_RETURN()) &&
-                        ! token.getToken().equals(lexicalAnalyzer.getNEW_LINE()) &&
-                        ! token.getToken().equals(lexicalAnalyzer.getVERTICAL_TAB()) &&
-                        !token.getToken().equals(lexicalAnalyzer.getSIMPLE_COMMENT())){
-
-                    tokens.add(token);
-                }
-
-                token = lexicalAnalyzer.getNextToken();
-            }
-
-            tokens.add(token); // agrega token EOF a la lista de tokens
-
-            if(outputPath == null){
-
-                printResults(tokens); // Imprimo reusltados por consola
 
             }
-            else {
-                //Guardo resultados en archivo de salida
-                fileManager.saveResults(validTokenFormat(tokens));
+
+            // Verifico si el lexema es una palabra reservada
+            if (this.hashMap.containsKey(token.getLexeme())) {
+
+                //Si esta, entonces es una palabra reservada y cambio su token
+
+                token.setToken(token.getLexeme());
             }
 
-        }
-        catch (LexicalException exception){
+
+        } catch (LexicalException exception) {
 
 
             /*
@@ -136,94 +119,16 @@ public class SyntacticExecutor {
 
         }
 
-
-
-    }
-
-    /**
-     * Método que dado una lista de tokens finales, sera el encargado
-     * de escribirlos en consola o de escribirlos en un archivo en el caso
-     * de que este se haya dado como parametro de entrada
-     * @param tokenList lista con tokens de todo el archivo de entrada
-     * @throws IOException si no puedo escribir el archivo
-     * @author Yeumen Silva
-     */
-
-    private void printResults(List<Token> tokenList) {
-
-        /*
-        Llamo a función que convierte las instancias de clase token
-        a string con el formato pedido
-         */
-
-        List<String> validTokens = validTokenFormat(tokenList);
-
-        //Si no pasaron argumento de path de salida, etonces:
-
-        printByConsole(validTokens);
-
+        return token;
 
     }
-
-    /**
-     * Método que dado una lista de tokens finales, los convierte a string
-     * en el formato indicado por el documento de entrega
-     * @param tokenList lista con tokens de todo el archivo de entrada
-     * @return Lista con los tokens convertidos en string para imprimir
-     * @author Yeumen Silva
-     * @author Lucas Moyano
-     */
-
-    private List<String> validTokenFormat(List<Token> tokenList){
-
-        //Declaro lista de strings
-
-        List<String> validStrings = new ArrayList<>();
-
-        // Agrego encabezado
-        validStrings.add("CORRECTO: ANALISIS LEXICO");
-        validStrings.add("| TOKEN | LEXEMA | NUMERO DE LINEA (NUMERO DE COLUMNA) |");
-
-        /*
-        Recorro lista de tokens y los guardo en una lista con el
-        formato pedido
-         */
-
-        for (Token token : tokenList) {
-
-            validStrings.add("| " + token.getToken() + " | " + token.getLexeme() +
-                    " | LINEA " + token.getRow() + " (COLUMNA " + token.getColumn() + ") |");
-
-        }
-
-        return validStrings;
-
-    }
-
-    /**
-     * Método que dado una lista de strings con formato solicitado,
-     * escribe por consola los resultados
-     * @param stringTokens lista con strings
-     *en formato pedido(token,lexeme,row,column)
-     * @author Yeumen Silva
-     */
-    private void printByConsole(List<String> stringTokens){
-
-        //Imprimo reocrriendo la lista
-
-        for(String tokenString : stringTokens){
-            System.out.println(tokenString);
-        }
-
-    }
-
 
     /**
      * Método que dado un error, lo imprime con el formato pedido
      * @param exception error de tipo léxico
      * @author Yeumen Silva
      */
-    private void printException(LexicalException exception){
+    private void printException(LexicalException exception) {
 
         Token tokenException = exception.getToken();
 
@@ -232,7 +137,11 @@ public class SyntacticExecutor {
 
         System.out.println("| Linea " + tokenException.getRow() +
                 " | COLUMNA " + tokenException.getColumn() +
-                " | " + exception.getExceptionType() + " " + tokenException.getLexeme()  );
+                " | " + exception.getExceptionType() + " " + tokenException.getLexeme());
 
     }
+
 }
+
+
+
