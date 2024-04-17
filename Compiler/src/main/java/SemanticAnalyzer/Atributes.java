e3635dd25e2080f70445a2c8759bbf08a1f52238
Yeu07 <yeumensilva9@gmail.com>
2024-04-17 17:36:44 -0300

    Clases modeladas

---
+++ Compiler/src/main/java/SemanticAnalyzer/Atributes.java
@@ -0,0 +1,17 @@
+package SemanticAnalyzer;
+
+public class Atributes {
+
+    //Id que identifica el atributo
+    private String name;
+
+    //Tipo del atributo
+    private String type; //ToDo que pasaria con arrays? quizas modelar type Class
+
+    //Si es o no público
+    private boolean isPublic;
+
+    //Posición del atributp
+    private int pos;
+
+}
---
+++ Compiler/src/main/java/SemanticAnalyzer/Params.java
@@ -0,0 +1,14 @@
+package SemanticAnalyzer;
+
+public class Params {
+
+    //Id del parámetro
+    private String name;
+
+    //Tipo asociado al parámetro
+    private String type;
+
+    //Posicion del parámetro
+    private int pos;
+
+}
