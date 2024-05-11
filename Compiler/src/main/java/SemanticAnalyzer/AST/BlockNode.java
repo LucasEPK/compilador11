c16c7e80a6f56581311e227e40a5021911ec1894
Lucas Moyano <lucasmoyano11@outlook.it>
2024-05-11 10:51:07 -0300

    añadí manera de agregar una nueva sentencia

--- Compiler/src/main/java/SemanticAnalyzer/AST/BlockNode.java
+++ Compiler/src/main/java/SemanticAnalyzer/AST/BlockNode.java
@@ -1,6 +1,8 @@
 package SemanticAnalyzer.AST;
 
-import java.util.LinkedHashMap;
+import SemanticAnalyzer.SymbolTable.SymbolTable;
+
+import java.util.List;
 
 /**
  * Clase representate ToDo
@@ -9,5 +11,48 @@ import java.util.LinkedHashMap;
 
 public class BlockNode {
 
-    SentenceNode[] sentenceList;
+    private List<SentenceNode> sentenceList;
+
+    private SemanticContext currentContext;
+
+    /**
+     * En este constructor se establece el contexto de la tabla de simbolos en donde está el bloque
+     * @author Lucas Moyano
+     * */
+    public BlockNode(SymbolTable symbolTable) {
+        currentContext = new SemanticContext(symbolTable.getCurrentStruct(), symbolTable.getCurrentMethod());
+    }
+
+    /**
+     * Esta función se encarga de elegir el tipo de sentencia que se va a agregar en la lista
+     * después de eso la agrega a la sentenceList
+     * @param sentenceType el tipo de sentencia que se quiere en String
+     * @author Lucas Moyano
+     * */
+    public void addNewSentence(String sentenceType){
+        SentenceNode newSentence = null;
+
+        switch (sentenceType) {
+            case "Asignation":
+                newSentence = new AsignationNode(getCurrentContext());
+            case "ExpBin":
+                newSentence = new ExpBin(getCurrentContext());
+            case "ExpUn":
+                newSentence = new ExpUn(getCurrentContext());
+            case "IfThenElse":
+                newSentence = new IfThenElseNode(getCurrentContext());
+            case "Return":
+                newSentence = new ReturnNode(getCurrentContext());
+            case "While":
+                newSentence = new WhileNode(getCurrentContext());
+            default:
+                // TODO: error el tipo de sentencia no existe
+        }
+
+        sentenceList.add(newSentence);
+    }
+
+    public SemanticContext getCurrentContext() {
+        return currentContext;
+    }
 }
