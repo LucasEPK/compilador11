4f6f6b13a76725ba6846799efcc3d0d9d14ed003
Lucas Moyano <lucasmoyano11@outlook.it>
2024-05-16 09:28:23 -0300

    Revert "nuevo método comentado"
    This reverts commit 226f92ab0d26d4a8fe14af68d88f6cc8fd74034a.

--- Compiler/src/main/java/SemanticAnalyzer/AST/AST.java
+++ Compiler/src/main/java/SemanticAnalyzer/AST/AST.java
@@ -32,11 +32,6 @@ public class AST implements Commons {
         return  newBlock;
     }
     */
-
-    /**
-     * Agrega un bloque a la lista
-     * @param block bloque a agregar
-     */
     public void addBlock(BlockNode block) {
         blockList.add(block);
     }
