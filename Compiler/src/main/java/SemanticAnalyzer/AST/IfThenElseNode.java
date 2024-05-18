f0f2ebef2340884584904b37a8c705c211245b40
Yeu07 <yeumensilva9@gmail.com>
2024-05-18 13:49:44 -0300

    Consolidación IfThenElseNode

--- Compiler/src/main/java/SemanticAnalyzer/AST/IfThenElseNode.java
+++ Compiler/src/main/java/SemanticAnalyzer/AST/IfThenElseNode.java
@@ -1,6 +1,7 @@
 package SemanticAnalyzer.AST;
 
 
+import Exceptions.SemanticExceptions.AST.MultipleReturnType;
 import SemanticAnalyzer.SymbolTable.SymbolTable;
 
 /**
@@ -70,6 +71,49 @@ public class IfThenElseNode extends  SentenceNode implements Commons {
 
     @Override
     public void consolidate(AST ast) {
+
+        if(this.ifNode.getConsolidated() == false){
+            this.ifNode.consolidate(ast);
+        }
+        if(this.thenNode.getConsolidated() == false){
+            this.thenNode.consolidate(ast);
+        }
+        if(this.ifNode.getType().equals("Bool") == false){
+            //ToDo
+            //throw new NoBooleanCondition(this.whileNode.getToken());
+        }
+        this.setType(thenNode.getType());
+        this.setConsolidated(true);
+
+        if(this.elseNode != null){
+            if(this.elseNode.getConsolidated() == false){
+                this.elseNode.consolidate(ast);
+            }
+            //Si los tipos de else y de then son diferentes
+            if(!this.elseNode.getType().equals(this.thenNode.getType())){
+                //ToDo
+                //throw new MultipleReturnType(this.getToken());
+            }
+
+            //Si los tipos de else y de then no son void
+            if(!this.elseNode.getType().equals("void") && !this.thenNode.getType().equals("void")){
+                //Si los tipos de else y de then son iguales
+                if(this.thenNode.getType().equals(this.thenNode.getType())){
+                    //Seteo que el tipo del nodo if es el mismo que el tipo del nodo then
+                    this.setType(thenNode.getType());
+                }else {
+                    //ToDo
+                    //throw new MultipleReturnType(this.thenNode.getToken());
+                }
+
+            }else if(!this.thenNode.getType().equals("void")){
+                this.setType(this.thenNode.getType());
+            }else if(!this.elseNode.getType().equals("void")){
+                this.setType(this.elseNode.getType());
+            }else {
+                setType(this.thenNode.getType());
+            }
+        }
 
     }
 
