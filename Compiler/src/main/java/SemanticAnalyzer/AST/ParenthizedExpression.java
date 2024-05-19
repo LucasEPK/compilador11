795a163ccc011f9a9b138b6b9406f513a7888789
Yeu07 <yeumensilva9@gmail.com>
2024-05-19 19:09:00 -0300

    Ahora se imprime Json de expresionParentizada

--- Compiler/src/main/java/SemanticAnalyzer/AST/ParenthizedExpression.java
+++ Compiler/src/main/java/SemanticAnalyzer/AST/ParenthizedExpression.java
@@ -17,8 +17,36 @@ public class ParenthizedExpression extends PrimaryNode{
         return parenthizedExpression;
     }
 
+    /**
+     * Método que convierte un objeto a un string en formato JSON
+     * @param tabs Cantidad de tabulaciones a agregar al archivo JSON
+     * @return string en formato JSON
+     */
+
+    public String toJson(int tabs) {
+
+        String json = addtabs(tabs) + "{\n";
+        json += addtabs(tabs+1) + "\"nombre\": \"" + "ParenthizedExpression" + "\",\n";
+        json += addtabs(tabs+1) + "\"value\": \"" + getToken().getLexeme() + "\",\n";
+        json += addtabs(tabs+1) + "\"type\": \"" + getType() + "\",\n";
+        json += addtabs(tabs+1) + "\"parenthizedExpression\": " + parenthizedExpression.toJson(tabs+1) + "\n";
+        json += addtabs(tabs) + "}";
+        return json;
+
+    }
+
+    /**
+     * Método que agrega tabulaciones al archivo JSON
+     * @param tabs Cantidad de tabulaciones a agregar
+     * @return String con las tabulaciones agregadas
+     */
+
     @Override
     public String addtabs(int tabs) {
-        return "";
+        String s = "";
+        for (int i = 0; i < tabs; i++) {
+            s += "\t";
+        }
+        return s;
     }
 }
